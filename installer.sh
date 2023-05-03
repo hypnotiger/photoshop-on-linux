d4a21cf71f78369fb739f60ddbb2527504c312b0
tigeritest <59480088+tigeritest@users.noreply.github.com>
2023-05-03 23:18:37 +0300

    fix vkd3d typo

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ echo "Starting Adobe Photoshop CC 2021 (v22) installer..."
 echo ""
 sleep 1
 
-read -p "Give a name to the WINE Prefix that will be used. A folder with this name will be created in the current directory." prefix_name
+read -p "Give a name to the WINE Prefix that will be used. A folder with this name will be created in the current directory. " prefix_name
 export WINEPREFIX="$PWD/$prefix_name"
 
 
@@ -80,7 +80,7 @@ sleep 1
 sleep 1
 
 echo "Installing & configuring winetricks components..."
-./scripts/winetricks fontsmooth=rgb gdiplus msxml3 msxml6 atmlib corefonts dxvk vcrun2019 vcrun2012 vcrun2013 vcrun2010 vdk3d
+./scripts/winetricks fontsmooth=rgb gdiplus msxml3 msxml6 atmlib corefonts dxvk vcrun2019 vcrun2012 vcrun2013 vcrun2010 vkd3d
 sleep 1
 
 echo "Installing redist components..."
